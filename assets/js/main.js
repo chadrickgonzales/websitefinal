024eaed922f5b36dfde6320f2e6bcdcbd420b258
Chadrick <chadroelgonzx@gmail.com>
2025-03-11 19:35:48 +0800

    added checkout and container for items in cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,28 @@ function showSection(section) {
   document.querySelectorAll('.content-section').forEach(sec => sec.style.display = 'none');
   document.getElementById(section).style.display = 'block';
 }
+
+function openPopup() {
+  document.getElementById("paymentPopup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("paymentPopup").style.display = "none";
+}
+
+function changeQuantity(amount) {
+  let quantityInput = document.getElementById("quantity");
+  let totalAmount = document.getElementById("total");
+
+  let currentQuantity = parseInt(quantityInput.value);
+  let newQuantity = currentQuantity + amount;
+
+  if (newQuantity > 0) {
+      quantityInput.value = newQuantity;
+      totalAmount.innerText = `$${newQuantity * 10}`;
+  }
+}
+
+function removeItem() {
+  document.querySelector(".product-card").remove();
+}
