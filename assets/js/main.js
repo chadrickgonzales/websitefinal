077f3e11a2aaa97c9c4101eec32f232613835978
Chadrick <chadroelgonzx@gmail.com>
2025-03-11 08:34:41 +0800

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+let list = document.querySelectorAll(".navigation li");
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove("hovered");
+  });
+  this.classList.add("hovered");
+}
+
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
+
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
+
+
+function showSection(section) {
+  document.querySelectorAll('.content-section').forEach(sec => sec.style.display = 'none');
+  document.getElementById(section).style.display = 'block';
+}
